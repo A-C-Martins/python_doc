8e7f9771e53b0bcc287181f7f69d3ef14b8e291b
AC Martins <arohchukwuebuka91@gmail.com>
2023-10-23 19:40:56 +0100

    tupple

---
+++ tupple/buffet.py
@@ -0,0 +1,14 @@
+
+print("Buﬀet style restaurant food list"); print("\n")
+
+food_list = ('pizza', 'Ice cream', 'Jellof', 'Shawama', 'Noodle')
+for food in food_list:
+    print(food)
+    
+print("Trying to modify the tupple list!")
+food_list[2] = 'Garri'
+
+print("Re-assigning and changing the food list")
+food_list = ('Beaf potage', 'Noodle', 'Rice cake', 'sunchi', 'Chocolate Pup')
+for food in food_list:
+    print(food)
