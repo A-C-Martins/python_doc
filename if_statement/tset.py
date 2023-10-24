b94d87c00c9eb903ab7c121e0fe6f0788f797fc8
AC Martins <arohchukwuebuka91@gmail.com>
2023-10-24 20:09:16 +0100

    alian waw

---
+++ if_statement/tset.py
@@ -0,0 +1,22 @@
+## 5-7. Favorite Fruit: Make a list of your favorite fruits, and then 
+# write a series of independent if statements that check for certain fruits in your list. 
+# Make a list of your three favorite fruits and call it favorite_fruits. Write ﬁve if statements. 
+# Each should check whether a certain kind of fruit is in your list. If the fruit is in your list, the if block 
+# should print a statement, such as You r eally lik e bananas!
+
+favourite_fruits = ['Water Mellon', 'Orange', 'Banana']
+
+if 'Water Mellon' in favourite_fruits:
+    print('I really like Water Mellon')
+
+if 'Cucumber' in favourite_fruits:
+    print('I really like Cucumber')
+
+if 'Orange' in favourite_fruits:
+    print('I really like Orange')
+
+if 'Banana' in favourite_fruits:
+    print('I really like Banana')
+
+if 'Apple' in favourite_fruits:
+    print('I really like an Apple')
