6fd3a220c23063904230e5698996b59a74299b9b
AC Martins <arohchukwuebuka91@gmail.com>
2023-10-27 20:34:48 +0100

    best number

---
+++ dictionary/favorite_number.py
@@ -0,0 +1,13 @@
+## 6-2. 
+# Favorite Numbers: Use a dictionary to store people’s favorite numbers. 
+# Think of ﬁve names, and use them as keys in your dictionary. 
+# Think of a favorite number for each person, and store each as a value in your dictionary.
+# Print each person’s name and their favorite number.
+# For even more fun, poll a few friends and get some actual data for your program. 
+
+best_number = {'Elon' : 42, 'mark' : 30, 'Jeff' : 12, 'Buffet' : 7, 'martins' : 21}
+print(f"Elon best number is {best_number['Elon']}")
+print(f"Mark best number is {best_number['mark']}")
+print(f"Jeff best number is {best_number['Jeff']}")
+print(f"Buffet best number is {best_number['Buffet']}")
+print(f"Martins best number is {best_number['martins']}")
