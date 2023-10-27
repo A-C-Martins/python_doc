8407903de49d9922e4d7d897eb23881c31a69949
AC Martins <arohchukwuebuka91@gmail.com>
2023-10-27 20:20:33 +0100

    commit

---
+++ dictionary/person.py
@@ -0,0 +1,10 @@
+## 6-1. Person: Use a dictionary to store information about a person you know. 
+# Store their ﬁrst name, last name, age, and the city in which they live. 
+# You should have keys such as first_name, last_name, age, and city. 
+# Print each piece of information stored in your dictionary. 
+
+friend = {'first_name' : 'Elon', 'last_name' : 'Musk', 'age' : 35, 'city' : 'Washinton DC'}
+print(friend['first_name'])
+print(friend['last_name'])
+print(friend['age'])
+print(friend['city'])
